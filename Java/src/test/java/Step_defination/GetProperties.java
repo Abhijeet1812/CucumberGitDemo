8c8f333c1e321b44d9701fe09495100a29c9963e
Abhijeet <sabale.abhijeet2@gmail.com>
2022-01-04 15:01:01 +0530

    hello this is my 3rd time to doing this

--- Java/src/test/java/Step_defination/GetProperties.java
+++ Java/src/test/java/Step_defination/GetProperties.java
@@ -7,6 +7,20 @@ public class GetProperties {
 	System.out.println("hello Get");
 	System.out.println("hello Get");
 	System.out.println("hello Get");
+
+	System.out.println("hello Get1");
+
+	System.out.println("hello Get2");
+
+	System.out.println("hello Get3");
+
+	System.out.println("hello Get4");
+
+	System.out.println("hello Ge5t");
+
+	System.out.println("hello Get6");
+
+	System.out.println("hello Get7");
 	
 
 }
