637ffa28661eb7289eff77c6da911bd113ff94b8
Abhijeet <sabale.abhijeet2@gmail.com>
2022-01-04 14:40:15 +0530

    this is second time i am doing and asian

--- Java/src/test/java/Step_defination/GetProperties.java
+++ Java/src/test/java/Step_defination/GetProperties.java
@@ -2,5 +2,11 @@ package Step_defination;
 
 public class GetProperties {
 	final String url ="https://rahulshettyacademy.com/seleniumPractise/#/";
+	System.out.println("hello Get");
+	System.out.println("hello Get");
+	System.out.println("hello Get");
+	System.out.println("hello Get");
+	System.out.println("hello Get");
+	
 
 }
